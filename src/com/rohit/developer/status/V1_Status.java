fb983220bc7144fe2d92c1be71c816f9720bfb43
Rohit261189 <chaturvedirohit1189@gmail.com>
2015-11-23 17:43:22 +0530

    finished episode 2 of tutorial

--- src/com/rohit/developer/status/V1_Status.java
+++ src/com/rohit/developer/status/V1_Status.java
@@ -7,11 +7,21 @@ import javax.ws.rs.core.MediaType;
 @Path("/v1/status")
 public class V1_Status {
 	
+	private static final String api_version="00.01.00";
+	
 	@GET
 	@Produces(MediaType.TEXT_HTML)
 	public String returnTitle()
 	{
 		return "<p>Java Web Service</p>";
+	}
+	
+	@Path("/version")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnVersion()
+	{
+		return "Version:"+api_version;
 	}
 
 }
